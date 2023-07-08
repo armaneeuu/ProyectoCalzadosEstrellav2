d7e912390daa47fdc8a61646a54bbb94e8d63a01
Armando Velarde <armaninteresado@hotmail.com>
2023-07-08 18:50:03 -0500

    Agregar BD y conexión

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql("PostgressConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgressConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
