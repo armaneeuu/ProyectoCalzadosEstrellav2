110084538b0fe4d1672a10ed70a8fc436e737c7c
Armando Velarde <armaninteresado@hotmail.com>
2023-07-08 17:06:41 -0500

    Primera subida de código; Register y Login

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoCalzadosEstrella.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
